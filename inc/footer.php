d274f03d51e096c01a6813336671917b508490a4
Saikatadhurya <sakatadhuryabirds@gmail.com>
2019-06-02 12:20:54 +0530

    On branch master

--- inc/footer.php
+++ inc/footer.php
@@ -2,4 +2,5 @@
      <h2>Get in Your Mobile</h2> 
 	<p class="lead">Click on the Link Below and Enjoy the features of this awesome app.</p>
 	<p><a href=""><img id="appStoreIcon" src="image/apps.png"></a></p>
+	Designed and Developed by: Saikat Adhurya
 </div>

dca7aaea488a334080a570b03b88d056e6833b80
Saikatadhurya <sakatadhuryabirds@gmail.com>
2019-06-02 12:55:00 +0530

    On branch master

--- index.php
+++ index.php
@@ -168,7 +168,7 @@ $search = new Search($db);
 <hr></div>
 <div class="container">
 	<div  id="appSummary">
-	<h1 class="shadow p-3 mb-5 bg-white rounded" style="border-style: solid;border-width: 3px; padding:10px;margin-top:100px;">Explore Some Awesome <i class="fab fa-app-store"></i>pps Here </h1>
+	<h1 class="shadow p-3 mb-5 bg-white rounded" style="border-style: solid;border-width: 3px; padding:10px;margin-top:100px;">Explore Here Some Awesome  <i class="fab fa-app-store"></i>pps !!</h1>
 	<p>Download Now For FREE !!!</p>
 	</div>
 	<div class="row">
